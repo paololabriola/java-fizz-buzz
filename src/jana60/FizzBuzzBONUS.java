7218646f95f5874d00fa63981991806c1ca5a4ea
paololabriola <paololabriola02@gmail.com>
2022-06-09 15:21:39 +0200

    Improved code

--- src/jana60/FizzBuzzBONUS.java
+++ src/jana60/FizzBuzzBONUS.java
@@ -6,7 +6,7 @@ public class FizzBuzzBONUS {
 	
 	public static void main(String[] args) {
 		
-		int numeroMassimo=0;
+		int numeroMassimo=0, r3, r5;
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.print("Inserisci un numero tra 1 e 200, che andrà a definire il numero massimo da raggiungere: ");
@@ -20,24 +20,22 @@ public class FizzBuzzBONUS {
 		}
 		
 		for(int i=1; i<=numeroMassimo; i++) {
-		
-			if((i%15) == 0) {
 			
-				System.out.println("FizzBuzz");
+			String risultato="";
 			
-			}else if((i%3) == 0) {
+			r3 = i % 3;
+			r5 = i % 5;
 			
-				System.out.println("Fizz");
+			if(r3 == 0) 
+				risultato += "Fizz";
 			
-			}else if((i%5) == 0){
-			
-				System.out.println("Buzz");
-			
-			}else {
-			
-				System.out.println(i);
-			
-			}
+			if(r5 == 0)
+				risultato += "Buzz";
+				
+			if((r3 != 0) && (r5 != 0))
+				risultato += i;
+				
+			System.out.println(risultato);
 		
 		}
 
